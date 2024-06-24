5af822ba1d006c5ed33fb0f1ccf0b29e4edfb25e
bogachukmax <128833332+bogachukmax@users.noreply.github.com>
2024-06-24 22:53:31 +0300

    Delete Spotify-main directory

--- Spotify-main/script/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const logInBtn = document.getElementById('logBtn')
-    const toReg = document.getElementById('swapToRegister')
-
-    toReg.addEventListener('click', () => {
-        logPage.classList.toggle('displayNone')
-        regPage.classList.toggle('displayBlock')
-    })
-})
