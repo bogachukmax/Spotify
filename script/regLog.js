afd0d50d5fccdbef8cfe1433a30cc4e3a64b3512
itstepkiri <dotdot2132@gmail.com>
2024-06-25 01:29:02 +0300

    fix/ added changes on reg

--- script/regLog.js
+++ script/regLog.js
@@ -2,16 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const logInBtn = document.getElementById('logBtn')
     const toReg = document.getElementById('swapToRegister')
     const textReg = document.querySelector('.textReg')
+    const regInps = document.querySelector('.regInps')
     const forHr = document.querySelector('.forHr')
-    const int = document.getElementById('.int')
+    const int = document.querySelector('.int')
     const forHr2 = document.querySelector('.forHr2')
     const backToLog = document.querySelector('.backToLog')
+    const passCreate = document.querySelector('.passCreate')
 
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve()
             reject('Error')
-        }, 2000);
+        }, 0);
     })
     .then(() => {
         return new Promise((resolve, reject) => {
@@ -19,13 +21,38 @@ document.addEventListener('DOMContentLoaded', () => {
             const button = document.getElementById('regNext')
             const error = document.getElementById('forError')
             button.addEventListener('click', () => {
-                let a = button.value
-                if(!a.includes('@')){
-                    error.textContent = `E-mail must contain '@'`
+                let a = emailReg.value
+                if(!a.includes('@') || a.length < 5){
+                    emailReg.style.borderColor = `red`
+                    error.style.display = `block`
+                    reject('error')
+                } else{
+                    emailReg.style.borderColor = `lightgray`
+                    error.style.display = `none`
+                    forHr.style.display = `none`
+                    int.style.display = `none`
+                    forHr2.style.display = `none`
+                    backToLog.style.display = `none`
+                    regInps.style.display = `none`
+                    textReg.style.display = `none`
+                    passCreate.style.display = `flex`
+                    const dataUser = {email: a}
+                    resolve(dataUser)
                 }
             })
         })
     })
 
-    regUser(emailReg.value)
+    .then((dataUser) => {
+        return new Promise((resolve, reject) => {
+            
+        })
+    })
+
+    
+
+
+    .catch((err) => {
+        console.log(err);
+    })
 })
