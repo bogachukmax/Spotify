d7192aecd5740adc78dac929a97929e502b14653
JOider1 <stasbondar4532gmail.com>
2024-06-27 22:55:18 +0300

    f

--- script/main.js
+++ script/main.js
@@ -17,36 +17,36 @@
 //     });
 // });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const data = JSON.parse(localStorage.getItem('userData'))
-//     console.log(data);
-//     const userName = document.getElementById('userNameUp')
-//     const userDate = document.getElementById('userDateUp')
-//     const exit = document.getElementById('exitFromAcc')
-//     const deleteAcc = document.getElementById('deleteAcc')
-//     const confDel = document.getElementById('confirmDelete')
-//     const delPage = `../html/deleteAcc.html`
-//     const logPage = `../html/logIn.html`
-//     const name = data.userName
-//     const date = data.dateOfBirth
-//     if(true){
-//         userName.textContent = name
-//         userDate.textContent = date
-//     }
+document.addEventListener('DOMContentLoaded', () => {
+    const data = JSON.parse(localStorage.getItem('userData'))
+    console.log(data);
+    const userName = document.getElementById('userNameUp')
+    const userDate = document.getElementById('userDateUp')
+    const exit = document.getElementById('exitFromAcc')
+    const deleteAcc = document.getElementById('deleteAcc')
+    const confDel = document.getElementById('confirmDelete')
+    const delPage = `../html/deleteAcc.html`
+    const logPage = `../html/logIn.html`
+    const name = data.userName
+    const date = data.dateOfBirth
+    if(true){
+        userName.textContent = name
+        userDate.textContent = date
+    }
 
-//     exit.addEventListener('click', () => {
-//         if(true){
-//             location.href = logPage
-//         }
-//     })
+    exit.addEventListener('click', () => {
+        if(true){
+            location.href = logPage
+        }
+    })
 
-//     deleteAcc.addEventListener('click', () => {
-//         if(true){
-//             location.href = delPage
-//             return
-//         }
-//     })
-// })
+    deleteAcc.addEventListener('click', () => {
+        if(true){
+            location.href = delPage
+            return
+        }
+    })
+})
 
 
 document.addEventListener('DOMContentLoaded', () => {
